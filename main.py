5fefffc5128d9e913e626b4ad4503f091b8289dd
ericw <ericw@marketamerica.com>
2021-02-10 17:32:29 -0800

    change

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ cb = cluster.bucket('session')
 app = Flask(__name__)
 
 
-@app.route('/<pc_id>', methods=['GET'])
+@app.route('/customer_profile/<pc_id>', methods=['GET'])
 def hello_world(pc_id):
     try:
         document = cb.get('customer::profile::' + pc_id).value[0]
@@ -21,17 +21,14 @@ def hello_world(pc_id):
         return ''
 
 
-@app.route('/<pc_id>', methods=['POST'])
+@app.route('/customer_profile/<pc_id>', methods=['POST'])
 def update(pc_id):
     data = request.get_json()
-    print(data['username'])
     key = 'customer::profile::' + pc_id
     print(key)
     try:
-        document = cb.get(key).value[0]
-        document["test"] = "test1111"
-        cb.upsert(key, [document])
-        return jsonify(document)
+        cb.upsert(key, [data])
+        return jsonify(data)
     except DocumentNotFoundException as e:
         print(e)
         return ''
